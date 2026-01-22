1cdbbe0ee5818cbcc992a2a0ebe09a7bd3f4cbcd
GianBGCS <gc.bigcas@gmail.com>
2026-01-22 22:01:20 +0800

    Add files via upload

---
+++ SecondCalcu.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SecondCalcu{
+    public static void main(Strings[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Menu: \n[1] Addition \n[2] Subtraction \n[3] Multiplication");
+        System.out.print("Choose an option: ");
+        int option = scan.nextInt;
+
+        
+    }
+}
