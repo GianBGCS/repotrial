ce056c24e605c999ec12f9da5b7682f755a059e7
gc.bigcas <gc.bigcas@gmail.com>
2026-01-22 22:29:39 +0800

    Added multiplication class

--- SecondCalcu.java
+++ SecondCalcu.java
@@ -40,4 +40,7 @@ public class SecondCalcu{
     public static double difference (double a, double b) {
         return a - b;
     }
+    public static double product (double a, double b) {
+        return a * b;
+    }
 }
