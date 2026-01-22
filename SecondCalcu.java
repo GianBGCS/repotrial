a798022518995cfc4c908b920cec03151b480e2b
gc.bigcas <gc.bigcas@gmail.com>
2026-01-22 22:17:33 +0800

    Added addition function

--- SecondCalcu.java
+++ SecondCalcu.java
@@ -4,9 +4,21 @@ public class SecondCalcu{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Menu: \n[1] Addition \n[2] Subtraction \n[3] Multiplication");
+        System.out.println("Menu: \n[1] Addition \n[2] Subtraction \n[3] MultiplicatiSon");
         System.out.print("Choose an option: ");
         int option = scan.nextInt();
+
+        switch (option){
+            case 1:
+                System.out.println("Addition");
+                System.out.print("Enter first number");
+                double add1 = scan.nextDouble();
+
+                System.out.print("Enter second number");
+                double add2 = scan.nextDouble();
+
+                System.out.println(sum(add1, add2));
+        }
 
     }
 
