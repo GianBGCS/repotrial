eed79f38373b5f89b308611bdad9c9abfd89be36
gc.bigcas <gc.bigcas@gmail.com>
2026-01-22 22:10:50 +0800

    Added addition class

--- SecondCalcu.java
+++ SecondCalcu.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 public class SecondCalcu{
-    public static void main(Strings[] args){
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Menu: \n[1] Addition \n[2] Subtraction \n[3] Multiplication");
         System.out.print("Choose an option: ");
-        int option = scan.nextInt;
+        int option = scan.nextInt();
 
-        
+    }
+
+    public static double sum (double a, double b) {
+        return a + b;
     }
 }
